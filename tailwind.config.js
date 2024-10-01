f4f67b8d81b810df9c630e8c292114575b021b4c
Austin Vach <austinvach@outlook.com>
2024-09-30 21:00:05 -0700

    Updated style and added sponsors page.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['index.html', 'live.html', 'podcasts.html', 'audible.html', 'nordvpn.html'],
+  content: ['*.html'],
   theme: {
     extend: {},
   },
